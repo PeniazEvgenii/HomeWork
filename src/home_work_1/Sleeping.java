c5c91f7650f3e89097e58f75b57d37eba87ce918
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-02-18 15:50:23 +0300

    Task 6 is completed

---
+++ src/home_work_1/Sleeping.java
@@ -0,0 +1,24 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+public class Sleeping {
+    public static void main(String[] args) {
+        boolean result = sleepIn(true,false);
+
+        if (result) {
+            System.out.println("Можем спать дальше");
+        } else {
+            System.out.println("Вставай и иди на работу!");
+        }
+    }
+
+    public static boolean sleepIn (boolean weekday, boolean vokation) {
+        boolean rest = false;
+
+        if (!weekday || vokation) {
+            rest = true;
+        }
+        return rest;
+    }
+}
