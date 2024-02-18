99e2a3577b5a7458d1a1c0330acfbb81d3edbd53
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-02-18 17:23:22 +0300

    Task 7 v2.0 is completed

--- src/home_work_1/Phone.java
+++ src/home_work_1/Phone.java
@@ -2,30 +2,26 @@ package home_work_1;
 
 public class Phone {
     public static void main(String[] args) {
-        int[] mass = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        String pnoneNum = createPhoneNumber(mass);
-        System.out.println(pnoneNum);
+        int[] mass = {1,2,3,4,5,6,7,8,9,0};
+
+        String phoneNum = createPhoneNumber(mass);
+        System.out.println(phoneNum);
     }
     public static String createPhoneNumber (int [] arr) {
         String format = "(";
 
-        for (int i = 0; i < arr.length - 1; i++) {     // исправить. почему-то с 1
+        for (int i = 0; i < arr.length; i++) {
             if (i >= 0 && i <= 2) {
-                System.out.println(arr[i]);
                 format += arr[i];
                 if (i == 2) {
                     format += ") ";
                 }
-            }
-
-            if (i >= 3 && i <= 5) {
+            } else if (i >= 3 && i <= 5) {
                 format += arr[i];
                 if (i == 5) {
                     format += "-";
                 }
-            }
-
-            if (i >= 6 && i <= 9) {
+            } else if (i >= 6 && i <= 9) {
                 format += arr[i];
             }
         }
