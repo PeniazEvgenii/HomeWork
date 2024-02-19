d67a0df60f0427baaf7ffcab121249108e0cfd82
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-02-19 19:24:35 +0300

    Task 8 v2.0 is completed

--- src/home_work_1/BinConverter.java
+++ src/home_work_1/BinConverter.java
@@ -6,15 +6,29 @@ public class BinConverter {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите число от -128 до 127");
-        byte number = console.nextByte();
+        byte number;
 
-        System.out.println("двоичное представление числа " + number + " состовляет " + toBinaryString(number));
+        if (console.hasNextByte()) {
+            number = console.nextByte();
+            System.out.println("двоичное представление числа " + number + " составляет " + toBinaryString(number));
+        } else {
+            System.out.println("Введено число не входящее в интервал от -128 до 127. Запустите программу заново");
+            System.exit(0);
+
+        }
+
     }
 
     public static String toBinaryString (byte number) {
         String bin = "";
         boolean minus = number < 0;
-        int temp = Math.abs(number);
+        int temp;
+
+        if (minus) {
+            temp = 256 + number;
+        } else {
+            temp = number;
+        }
 
         for (int i = 0; temp > 1 ; i++) {
             bin = temp % 2 + bin;
@@ -22,18 +36,11 @@ public class BinConverter {
         }
         bin = temp + bin;
 
-        for (int i = 0; i < 8; i++) {   // дополним нулями до 8 бит
+        for (int i = 0; i < 8; i++) {
             if (bin.length() <= i) {
                 bin = "0" + bin;
             }
         }
-
-        if (minus) {
-            bin =  bin.replace('1','2').replace('0','1').replace('2','0'); // обратный код
-            // как то прибавить 1 надо!!!!
-            // подумать над разницей pow(2, 8)
-        }
-
 
         return bin;
     }
