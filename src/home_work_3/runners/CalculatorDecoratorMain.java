155ba714bda7276fc58397639c5e0f473bbaf06e
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-03-19 11:34:01 +0300

    Deleted and correct comment

--- src/home_work_3/runners/CalculatorDecoratorMain.java
+++ src/home_work_3/runners/CalculatorDecoratorMain.java
@@ -21,6 +21,7 @@ public class CalculatorDecoratorMain {
        if (calc instanceof CalculatorWithCounterAutoDecorator) {
           if ((((CalculatorWithCounterAutoDecorator) calc).getCalculator()) instanceof CalculatorWithMemoryDecorator) {
              ((CalculatorWithMemoryDecorator) ((CalculatorWithCounterAutoDecorator) calc).getCalculator()).save();
+              System.out.print("Результат последней операции сохраненной в памяти  ");
              System.out.println(((CalculatorWithMemoryDecorator) ((CalculatorWithCounterAutoDecorator) calc).getCalculator()).load());
           }
        }
@@ -29,21 +30,18 @@ public class CalculatorDecoratorMain {
        * Оператор instance of проверяет происхождение объекта. т.е. был ли объект, на который ссылается переманная, создан на основе какого-либо класса.
        * Мы создаем обобщенную ссылку calc типа ICalculator. В нашем случае у Icalculator нет абстрактных методов load(), save(), getCalculator().
        * Чтобы к ним обратиться нужно использовать приведение объекта к типу (классу) CalculatorWithMemoryDecorator, т.к. в нем эти методы
-       * реализованы. С нисходящим приведением, мы должны быть уверены в принадлежности объекта, ведь если мы неверно приведем типы и вызовем метод, то возникнет ошибка компиляции.
+       * реализованы. С нисходящим приведением, мы должны быть уверены в принадлежности объекта, ведь если мы неверно приведем типы и вызовем метод, то возникнет ошибка в виде исключения "Exception in thread "main".
        */
 
-
-       //  System.out.println("Тест с сохранением рез-та после каждого действия с помощью приведения");
-       //  calc.getDivision(28, 5);
-       //  ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();                                    //??? у нас не объявлены методы в интерфейсе для функций памяти. объявить. что делать с остальными реализациями???
-       //  calc.getPowerOfNumber(((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load(),2);           // дефолтный метод у интерфейса может?  В примерах есть абстрактный родитель и объкт создается его типа а обертки его наследники
-       //  ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();                                    // если дефолтными сделать то load идет в интерфейс а не в memoryDecorate
-       //  calc.getAddition(4.1, ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load());
-       //  ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();
-       //  calc.getAddition(((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load(), calc.getMultiplicatin(15, 7));
-       //  ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();
-       //  System.out.printf("4.1 + 15 * 7 + (28 / 5) ^ 2 =  %.4f%n", ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load());
-
-
+        System.out.println("Тест с сохранением рез-та после каждого действия с помощью приведения");
+        calc.getDivision(28, 5);
+        ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();
+        calc.getPowerOfNumber(((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load(),2);
+        ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();
+        calc.getAddition(4.1, ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load());
+        ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();
+        calc.getAddition(((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load(), calc.getMultiplicatin(15, 7));
+        ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).save();
+        System.out.printf("4.1 + 15 * 7 + (28 / 5) ^ 2 =  %.4f%n", ((CalculatorWithMemoryDecorator)((CalculatorWithCounterAutoDecorator)calc).getCalculator()).load());
     }
 }
